b06e3c56f38439bb811c4115a56f664083a60c46
James Lewis <jamesjw2005@gmail.com>
2024-09-02 09:51:45 +0530

    Using Bufferclass (Experiment_2)

---
+++ SEM III/OOPJ/Experiment_2/code_2.java
@@ -0,0 +1,18 @@
+//Using BufferClass
+
+Code Using BufferClass:
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReader 
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter your name:");
+        
+            String name = reader.readLine();
+            System.out.println("Hello, " + name);
+    }
+}
