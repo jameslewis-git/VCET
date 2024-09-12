ad34fc8696705bb5d1cef147406d25344c4ba756
James Lewis <jamesjw2005@gmail.com>
2024-09-12 14:05:34 +0530

    To use packages in java

---
+++ SEM III/OOPJ/Experiment 5/Code.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Test 
+
+{
+	public static void main(String args[])
+	{
+		double number = 64.0;
+		double squareRoot = Math.sqrt(number);
+		System.out.println("The square root of " + number + " is: " + squareRoot);
+	}
+}
